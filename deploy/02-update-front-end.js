aab157fb0cb3e2842fd29997241c769dedb6c504
Andy Hoffman <antibland@gmail.com>
2022-10-26 01:32:33 -0700

    chore: update front-end deploy script

--- deploy/02-update-front-end.js
+++ deploy/02-update-front-end.js
@@ -1,6 +1,6 @@
 const { frontEndContractsFile, frontEndAbiFile } = require("../helper-hardhat-config")
 const fs = require("fs")
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
 
 module.exports = async () => {
     if (process.env.UPDATE_FRONT_END) {
